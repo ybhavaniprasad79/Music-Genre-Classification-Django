21f5abe4f5fe7f3d6aff7be580d3b1547b9efbff
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2026-01-30 10:35:04 +0530

    Fix scikit-learn build with --no-build-isolation

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ set -o errexit
 # Upgrade pip
 pip install --upgrade pip
 
-# Install numpy first (required by scikit-learn 0.22.1)
-pip install "numpy>=1.18.0"
+# Install build dependencies first
+pip install "numpy>=1.18.0" "scipy>=0.17.0" "Cython<3.0"
 
-# Install all other requirements
-pip install -r requirements.txt
+# Install scikit-learn without build isolation
+pip install --no-build-isolation "scikit-learn==0.22.1"
+
+# Install all other requirements (skip scikit-learn since already installed)
+pip install -r requirements.txt --ignore-installed scikit-learn
 
 python manage.py collectstatic --no-input
 python manage.py migrate
