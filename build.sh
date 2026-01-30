bb013bea12f5fb8dcefde2471beb27ec4c19eafd
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2026-01-30 10:37:21 +0530

    Simplify build script - use Python 3.9 for sklearn compatibility

--- build.sh
+++ build.sh
@@ -5,14 +5,8 @@ set -o errexit
 # Upgrade pip
 pip install --upgrade pip
 
-# Install build dependencies first
-pip install "numpy>=1.18.0" "scipy>=0.17.0" "Cython<3.0"
-
-# Install scikit-learn without build isolation
-pip install --no-build-isolation "scikit-learn==0.22.1"
-
-# Install all other requirements (skip scikit-learn since already installed)
-pip install -r requirements.txt --ignore-installed scikit-learn
+# Install all requirements
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
