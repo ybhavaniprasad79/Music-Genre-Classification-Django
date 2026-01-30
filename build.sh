fa716b45b5b48a9e74b46eeca4e6c79d180ea6bc
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2026-01-30 10:46:10 +0530

    Add wheel and scipy to build dependencies

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -o errexit
 # Upgrade pip
 pip install --upgrade pip
 
-# Install numpy first (scikit-learn 0.22.1 needs it for building)
-pip install "numpy<1.20"
+# Install build dependencies first
+pip install wheel setuptools
+pip install "numpy<1.20" "scipy<1.6"
 
 # Install scikit-learn separately with no build isolation
 pip install --no-build-isolation "scikit-learn==0.22.1"
