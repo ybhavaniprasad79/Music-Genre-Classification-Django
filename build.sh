51898c8597f6efd15005f9ce09750b07f4bb0152
ybhavaniprasad79 <ybhavaniprasad79@gmail.com>
2026-01-30 10:28:17 +0530

    Fix numpy dependency order for Render deployment

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip
+pip install --upgrade pip
+
+# Install numpy first (required by scikit-learn 0.22.1)
+pip install "numpy>=1.18.0"
+
+# Install all other requirements
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
